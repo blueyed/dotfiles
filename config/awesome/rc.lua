9c6e75d5b1d433c3427b0bf513a0d094004bffb2
Daniel Hahler <git@thequod.de>
2010-06-01 01:04:48 +0200

    Autostart for awesome.

--- config/awesome/rc.lua
+++ config/awesome/rc.lua
@@ -336,3 +336,34 @@ end)
 client.add_signal("focus", function(c) c.border_color = beautiful.border_focus end)
 client.add_signal("unfocus", function(c) c.border_color = beautiful.border_normal end)
 -- }}}
+
+
+-- Autostart
+-- via http://awesome.naquadah.org/wiki/Autostart#Directory_way
+-- for j in ${(f)"$(for i in ~/.config/autostart/*.desktop; do grep '^Exec=' $i | sed 's/^Exec=//'; done)"}; do $j &| ; done
+function autostart(dir)
+    if not dir then
+        do return nil end
+    end
+    local fd = io.popen("/bin/ls -1 -F " .. dir)
+    if not fd then
+        do return nil end
+    end
+    for file in fd:lines() do
+        local c= string.sub(file,-1)   -- last char
+        if c=='*' then  -- executables
+            executable = string.sub( file, 1,-2 )
+            print("Awesome Autostart: Executing: " .. executable)
+            awful.util.spawn_with_shell(dir .. "/" .. executable .. "") -- launch in bg
+        elseif c=='@' then  -- symbolic links
+            print("Awesome Autostart: Not handling symbolic links: " .. file)
+        else
+            print ("Awesome Autostart: Skipping file " .. file .. " not executable.")
+        end
+    end
+    io.close(fd)
+end
+
+autostart_dir = os.getenv("HOME") .. "/.config/autostart"
+autostart(autostart_dir)
+

458012fcd2918f1d8040141e12e02695f84b5035
Daniel Hahler <git@thequod.de>
2011-04-13 01:35:41 +0200

    Add script to find inode usage hogs.

---
+++ usr/bin/find_inode_usage.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+#
+# Find directories using the most inodes.
+#
+# Source: http://serverfault.com/questions/38907/determine-location-of-inode-usage/38912#38912
+
+find . -type d | 
+while 
+  read line  
+do 
+  echo "$( find "$line" -maxdepth 1 | wc -l) $line"  
+done | 
+sort -rn | less

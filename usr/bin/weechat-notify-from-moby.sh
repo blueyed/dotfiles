cd4bc25ab9f3187bb027c069e5b0a384d059cab0
Daniel Hahler <git@thequod.de>
2011-12-08 22:55:26 +0100

    Add weechat-notify-from-moby script.
    This displays highlights/mentions from a remote weechat instance locally.
    
    After all, it just `tail -f`s a logfile via SSH and notifies about new
    lines.

---
+++ usr/bin/weechat-notify-from-moby.sh
@@ -0,0 +1,48 @@
+#!/bin/sh
+# 
+# Connect remotely (via SSH) to a host running weechat and tail-f/read the logfile
+# containing any highlights.
+# For any new highlights/mentions a notification gets displayed (locally).
+
+export SSH_ASKPASS=ssh-askpass
+if ! ssh-add -l >/dev/null 2>&1; then ssh-add; fi
+
+logfile=/tmp/log.weechat
+chmod 600 $logfile
+
+# user and hosts information, encrypted.
+userhost=$(dotfiles-decrypt 'U2FsdGVkX1/IGyB2mLXvSSKSNFs/J8d7FiQwB2fx9HWU9w+1L9+v3SfWzYpkLlbk')
+internalhost=$(dotfiles-decrypt 'U2FsdGVkX1+t47mSzfhcSOzSjC73h5kGVDPbDhbXzRk=')
+
+# this might be used to override $autossh_weechat_port
+test -f ~/.dotfiles/dotfilesrc && source ~/.dotfiles/dotfilesrc
+
+if [ x$autossh_weechat_port = x ]; then
+  # autogenerate port based on hostname
+  autossh_weechat_port=$(( 20000 + $(sumcharvals $(hostname)) ))
+fi
+
+date >> $logfile
+echo "Starting loop in $0..." >> $logfile
+
+while true ; do
+    autossh -M $autossh_weechat_port $userhost \
+      ssh $internalhost "tail -n0 -f .weechat/logs/perl.strmon.weechatlog" < /dev/null | \
+        # sed -u 's/[<@&]//g' | \
+        while read date time number channel nick delim message; do
+          echo "== $(date) ==" >> $logfile
+          echo date time number channel nick delim message >> $logfile
+          echo "$date" "$time" "$number" "$channel" "$nick" "$delim" "$message" >> $logfile
+
+          title="${nick} @ ${channel}"
+          body="$message"
+          notify-send --category=im.received -t 15000 "$title" "$body"
+          # kdialog --passivepopup  "${message}" --title "${nick} @ ${channel}" 30
+          # zenity --info --title "${nick} @ ${channel}" --timeout 30 --text "${message}" 
+          # nervt, irgendwie einschränken:
+          #       ogg123 /usr/share/sounds/KDE-Sys-App-Message.ogg
+        done
+
+  echo "Sleeping 30 seconds after read failure..." >> $logfile
+  sleep 30
+done

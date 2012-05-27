1647f7f850c0632c62f0cadcfe0baae856f911d8
Daniel Hahler <git@thequod.de>
2012-05-28 00:24:28 +0200

    find_inode_usage: formatting, support for $1

--- usr/bin/find_inode_usage.sh
+++ usr/bin/find_inode_usage.sh
@@ -4,10 +4,12 @@
 #
 # Source: http://serverfault.com/questions/38907/determine-location-of-inode-usage/38912#38912
 
-find . -type d | 
-while 
-  read line  
-do 
-  echo "$( find "$line" -maxdepth 1 | wc -l) $line"  
-done | 
-sort -rn | less
+where="${1:-.}"
+
+find "$where" -xdev -type d |
+  while
+    read line
+  do
+    echo "$(find "$line" -maxdepth 1 | wc -l) $line"
+  done |
+    sort -rn | less

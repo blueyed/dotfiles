d2cf424b863e2e054324de82f3993a921e732602
Daniel Hahler <git@thequod.de>
2012-05-30 19:25:12 +0200

    Improve weechat-notify-from-moby script
     - play sound
     - kill autossh and subprocesses properly on exit/being killed, by using
       '-t' with autossh and setting up trap
     - slightly increment sleep time on read failure

--- usr/bin/weechat-notify-from-moby.sh
+++ usr/bin/weechat-notify-from-moby.sh
@@ -4,9 +4,17 @@
 # containing any highlights.
 # For any new highlights/mentions a notification gets displayed (locally).
 
+# Sound to play on highlight
+sound_message=/usr/share/sounds/ubuntu/stereo/message-new-instant.ogg
+
+# Export information required to play sound (via alsa/pulseaudio)
+export DISPLAY=:0
+export XAUTHORITY=/home/daniel/.Xauthority
+
 export SSH_ASKPASS=ssh-askpass
 if ! ssh-add -l >/dev/null 2>&1; then ssh-add; fi
 
+# XXX: why?
 PATH="$(dirname $0):$PATH"
 
 logfile=/tmp/log.weechat
@@ -26,27 +34,40 @@ if [ x$autossh_weechat_port = x ]; then
   autossh_weechat_port=$(( 20000 + port_offset ))
 fi
 
+# Kill autossh when this script gets killed
+export AUTOSSH_PIDFILE=/tmp/weechat-notify-from-moby-autossh.pid
+trap 'echo TRAP:$? >> $logfile ; test -f $AUTOSSH_PIDFILE && kill $(<$AUTOSSH_PIDFILE); exit;' 0 2 3 15
+
 date >> $logfile
 echo "Starting loop in $0..." >> $logfile
 
+sleep_failure=0
 while true ; do
-    autossh -M $autossh_weechat_port $userhost \
-      ssh $internalhost "tail -n0 -f .weechat/logs/perl.strmon.weechatlog" < /dev/null | \
-        # sed -u 's/[<@&]//g' | \
-        while read date time number channel nick delim message; do
-          echo "== $(date) ==" >> $logfile
-          echo date time number channel nick delim message >> $logfile
-          echo "$date" "$time" "$number" "$channel" "$nick" "$delim" "$message" >> $logfile
+  sleep_failure=$((sleep_failure+1))
+  autossh -M $autossh_weechat_port -t $userhost \
+    "ssh $internalhost 'tail -n0 -f .weechat/logs/perl.strmon.weechatlog'" | \
+    # sed -u 's/[<@&]//g' | \
+    while read date time number channel nick delim message; do
+      echo "== $(date) ==" >> $logfile
+      echo date time number channel nick delim message >> $logfile
+      echo "$date" "$time" "$number" "$channel" "$nick" "$delim" "$message" >> $logfile
 
-          title="${nick} @ ${channel}"
-          body="$message"
-          notify-send --category=im.received -t 15000 "$title" "$body"
-          # kdialog --passivepopup  "${message}" --title "${nick} @ ${channel}" 30
-          # zenity --info --title "${nick} @ ${channel}" --timeout 30 --text "${message}" 
-          # nervt, irgendwie einschränken:
-          #       ogg123 /usr/share/sounds/KDE-Sys-App-Message.ogg
-        done
+      title="${nick} @ ${channel}"
+      body="$message"
+      notify-send --category=im.received -t 15000 "$title" "$body"
+      # kdialog --passivepopup  "${message}" --title "${nick} @ ${channel}" 30
+      # zenity --info --title "${nick} @ ${channel}" --timeout 30 --text "${message}" 
+      # nervt, irgendwie einschränken:
+      #       ogg123 /usr/share/sounds/KDE-Sys-App-Message.ogg
 
-  echo "Sleeping 30 seconds after read failure..." >> $logfile
-  sleep 30
+      if [ -n "$sound_message" ]; then
+        if command -v play >/dev/null 2>&1 ; then
+          play "$sound_message"
+        fi
+      fi
+      sleep_failure=0 # reset on successful read
+    done
+
+  echo "Sleeping $sleep_failure seconds after read failure..." >> $logfile
+  sleep $sleep_failure
 done

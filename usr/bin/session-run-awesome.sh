f1e231e6309adaaca7dfccdcfd787dbfc574fe50
Daniel Hahler <git@thequod.de>
2017-05-24 21:57:50 +0200

    Update session-run-awesome.sh

--- usr/bin/session-run-awesome.sh
+++ usr/bin/session-run-awesome.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-# Gets run via /usr/share/applications/awesome-dev.desktop.
+# Gets run via ~/.xinitrc.
 #
 # Use SIGHUP to reload awesome.
 # Use SIGUSR to restart awesome.
@@ -33,8 +33,6 @@ unset TMUX
 # export XDG_CURRENT_DESKTOP=GNOME
 export XDG_CURRENT_DESKTOP=Unity
 
-loop_restart=1
-
 trap 'log "TRAPPED: USR1, restarting."; loop_restart=1; sleep_restart=0' USR1
 trap 'log "TRAPPED: TERM, not restarting."; loop_restart=0' TERM
 
@@ -42,13 +40,13 @@ trap 'log "TRAPPED: TERM, not restarting."; loop_restart=0' TERM
 export MY_SESSION_RUN_PID=$$
 log "MY_SESSION_RUN_PID=$MY_SESSION_RUN_PID"
 
-cd ~
+cd ~ || exit
 sleep_restart=1
 while :; do
 	log "=========== START AWESOME ==========="
 	date
 
-	loop_restart=0
+	loop_restart=1
 	last_start="$(date +%s)"
 
 	# Reset sleep when config file changed.
@@ -66,12 +64,14 @@ while :; do
 	fi
 	for f in /usr/local/bin/awesome /usr/bin/awesome; do
 		if [ -x "$f" ]; then
+			echo "Running: $f $args"
 			eval "$f $args"
 			RET=$?
 			echo "RET: $RET"
-			if [ "$RET" = 1 ]; then
-				# awesome crashed.
-				loop_restart=1
+
+			if [ "$RET" = 99 ]; then
+				# custom awesome.quit(99) call to indicate quitting.
+				loop_restart=0
 			fi
 			break
 		fi
@@ -81,12 +81,12 @@ while :; do
 		log "Parent PID ($PPID) is gone, not restarting.."
 		loop_restart=0
 
-	elif [ "$loop_restart" = 1 ]; then  # Set by 'trap'.
+	elif [ "$loop_restart" = 1 ]; then
 		# source ~/.xsessionrc
 		if [ "$(expr $(date +%s) - $last_start)" -lt 2 ]; then
 			if [ "$($STAT_CMD)" != "$last_stat" ]; then
 				sleep_restart=0
-			elif [ $(expr $sleep_restart \< 100) = 1 ]; then
+			elif [ "$(expr $sleep_restart \< 100)" = 1 ]; then
 				sleep_restart=$(expr $sleep_restart + 5)
 			fi
 		else

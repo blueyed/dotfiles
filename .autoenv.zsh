295590659097fefda594aded4d9ca4a79446d219
Daniel Hahler <git@thequod.de>
2018-09-16 05:04:18 +0200

    Remove .autoenv.zsh

--- .autoenv.zsh
+++
@@ -1,9 +0,0 @@
-if [[ $autoenv_event == enter ]]; then
-  if [[ -n $commands[ag] ]]; then
-    if [[ -n $aliases[ag] ]]; then
-      autostash alias ag="$aliases[ag] -U"
-    else
-      autostash alias ag='ag -U'
-    fi
-  fi
-fi
